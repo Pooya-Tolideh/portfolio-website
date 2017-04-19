2d28ff4657e6d193d0b75b44c1f7c8c78aaf579a
pooya-tolideh <pooya.tolideh@gmail.com>
2017-04-18 20:12:12 -0400

    mod :: refactored and overhauled the sidenav and scroll.js files using closures and callbacks to keep the global scope clean

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -16,76 +16,76 @@
 // 2. [codepen -- current solution](http://codepen.io/anon/pen/vmOpLX?editors=1010)
 // 3. using jQuery Animation
 
-    function smoothScroll(destination, duration, easing, callback) {
-    
-        duration = duration || 200;
-        easing = easing || 'linear';
+var smoothScroll = (function () {
+        return function (destination, duration, easing, callback) {
+            duration = duration || 200;
+            easing = easing || 'linear';
+
+            // Predefine list of available timing functions
+            // If you need more, tween js is full of great examples
+
+            var easings = {
+                linear : function (t) {
+                    return t;
+                },
+                easeInOutQuint : function (t) {
+                    return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
+                }
+            };
 
 
-        // Predefine list of available timing functions
-        // If you need more, tween js is full of great examples
-
-        var easings = {
-            linear : function (t) {
-                return t;
-            },
-            easeInOutQuint : function (t) {
-                return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * (--t) * t * t * t * t;
-            }
-        };
+            // Store initial position of a window and time
+            // If performance is not available in your browser
+            // It will fallback to new Date().getTime() - thanks IE < 10
+            var start = window.pageYOffset;
+            var startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
+            // var startTime = typeof(window.performance['now']) == 'function' ? performance.now() : new Date().getTime();
 
 
-        // Store initial position of a window and time
-        // If performance is not available in your browser
-        // It will fallback to new Date().getTime() - thanks IE < 10
-        var start = window.pageYOffset;
-        var startTime = 'now' in window.performance ? performance.now() : new Date().getTime();
-        // var startTime = typeof(window.performance['now']) == 'function' ? performance.now() : new Date().getTime();
+            // Take height of window and document to sesolve max scrollable value
+            // Prevent requestAnimationFrame() from scrolling below maximum scollable value
+            // Resolve destination type (node or number)
+            var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+            var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
+            var destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
+            var destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
 
 
-        // Take height of window and document to sesolve max scrollable value
-        // Prevent requestAnimationFrame() from scrolling below maximum scollable value
-        // Resolve destination type (node or number)
-        var documentHeight = Math.max(document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
-        var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.getElementsByTagName('body')[0].clientHeight;
-        var destinationOffset = typeof destination === 'number' ? destination : destination.offsetTop;
-        var destinationOffsetToScroll = Math.round(documentHeight - destinationOffset < windowHeight ? documentHeight - windowHeight : destinationOffset);
-
-
-        // If requestAnimationFrame is not supported
-        // Move window to destination position and trigger callback function
-        if ('requestAnimationFrame' in window === false) {
-            window.scroll(0, destinationOffsetToScroll);
-            if (callback) {
-                callback();
-            }
-            return;
-        }
-
-
-        // function resolves position of a window and moves to exact amount of pixels
-        // Resolved by calculating delta and timing function choosen by user
-        function scroll() {
-            var now = 'now' in window.performance ? performance.now() : new Date().getTime();
-            var time = Math.min(1, ((now - startTime) / duration));
-            var timeFunction = easings[easing](time);
-            window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
-
-            // Stop requesting animation when window reached its destination
-            // And run a callback function
-            if (window.pageYOffset === destinationOffsetToScroll) {
+            // If requestAnimationFrame is not supported
+            // Move window to destination position and trigger callback function
+            if ('requestAnimationFrame' in window === false) {
+                window.scroll(0, destinationOffsetToScroll);
                 if (callback) {
                     callback();
                 }
                 return;
             }
 
-            // If window still needs to scroll to reach destination
-            // Request another scroll invokation
-            requestAnimationFrame(scroll);
+
+            // function resolves position of a window and moves to exact amount of pixels
+            // Resolved by calculating delta and timing function choosen by user
+            function scroll() {
+                var now = 'now' in window.performance ? performance.now() : new Date().getTime();
+                var time = Math.min(1, ((now - startTime) / duration));
+                var timeFunction = easings[easing](time);
+                window.scroll(0, Math.ceil((timeFunction * (destinationOffsetToScroll - start)) + start));
+
+                // Stop requesting animation when window reached its destination
+                // And run a callback function
+                if (window.pageYOffset === destinationOffsetToScroll) {
+                    if (callback) {
+                        callback();
+                    }
+                    return;
+                }
+
+                // If window still needs to scroll to reach destination
+                // Request another scroll invokation
+                requestAnimationFrame(scroll);
+            }
+
+
+            // Invoke scroll and sequential requestAnimationFrame
+            scroll();
         }
-
-
-        // Invoke scroll and sequential requestAnimationFrame
-        scroll();
-    }
+    })();
--- src/scripts/side-nav.js
+++ src/scripts/side-nav.js
@@ -1,35 +1,51 @@
   //import smoothScroll from the global scope
   // Side Nav
-(function (smoothScroll) {
+function initSmoothScroll(smoothScroll) {
 
-    // 2. process every sideNav link
-    // and add scrolling func to it
-    function getLink(navItem) {
-    //get the link
-    var link = navItem.href;
-    // find the position of # in link
-    var pos = link.indexOf('#');
-    // get the id
-    var link = link.slice(pos + 1);
+    //first stage of processing using closure
+    return function () {
+         // 2. process every sideNav link
+         // and add scrolling func to it
 
-    // add a handler for click
-    navItem.addEventListener('click', function(){
-        // get the element with target id
-        var section = document.getElementById(link);
-        // scroll
-        smoothScroll(
-            section,
-            625,
-            'easeInOutQuint',
-            function() {console.log('Just finished scrolling to ' + window.pageYOffset + 'px')}
-        );
-        });
+         //second stage - adding handler to each item. 
+         // creating a separate scope using callback and closure
+        function getLink(navItem) {
+            //get the link
+            var link = navItem.href;
+            // find the position of # in link
+            var pos = link.indexOf('#');
+            // get the id
+            var link = link.slice(pos + 1);
+
+            //callback return for the second stage
+            // callback has access to all the variables
+            return function () {
+                // add a handler for click
+                navItem.addEventListener('click', function(){
+                    // get the element with target id
+                    var section = document.getElementById(link);
+                    // scroll
+                    smoothScroll(
+                        section,
+                        625,
+                        'easeInOutQuint',
+                        function() {console.log('Just finished scrolling to ' + window.pageYOffset + 'px')}
+                    );
+                });
+            }
+            
+        }
+         //firs stage
+         // 1. loop through every item in side-nav
+        //get each sideNav item
+        var sideNav = document.querySelectorAll('.m-side-nav__item');
+        for(i=0; i < sideNav.length; i++) {
+            // we refactoring this into a function so every element gets its own unique scope    
+            getLink(sideNav[i])();
+        }
     }
-    // 1. loop through every item in side-nav
-    //get each sideNav item
-    var sideNav = document.querySelectorAll('.m-side-nav__item');
-    for(i=0; i < sideNav.length; i++) {
-    // we refactoring this into a function so every element gets its own unique scope    
-    getLink(sideNav[i]);
-    }
-})(smoothScroll); 
+
+    
+       
+}
+initSmoothScroll(smoothScroll)(); 
